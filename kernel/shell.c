4a7f2c270b2cc7be6a1ac80ea4f54fddbea9d204
PortalDart <120784194+PortalDart@users.noreply.github.com>
2025-08-23 11:53:48 +0200

    updated shell.c

--- kernel/shell.c
+++ kernel/shell.c
@@ -11,6 +11,19 @@ char buffer[1000];
 
 size_t i = 0;
 
+enum tokens {
+    TOKEN_ECHO,
+    TOKEN_HELP,
+    TOKEN_INFO,
+    TOKEN_PARAMETER,
+    TOKEN_UNKNOWN
+};
+
+struct tokenType {
+    enum tokens tokens;
+    char *parameter;
+};
+
 void command() {
     buffer[i] = character;
     i++;
@@ -29,54 +42,27 @@ void command() {
     }
 }
 
+//shit
+enum tokens get_token(const char *c) {
+    if (strncmp(c, "echo", 4) == 0) {
+        return TOKEN_ECHO;
+    }
+    return TOKEN_UNKNOWN;
+}
+
 void shell(){
-    if (strcmp(buffer, "test\n") == 0) {
-        printk("\n", VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
-        printk("test\n", VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
-        printk("myos > ", VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
-    }
-    else if (strcmp(buffer, "help\n") == 0) {
-        printk("\n", 15, VGA_COLOR_BLACK);
-        printk("------------------------------------------------------\n", VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
-        printk("| test - test command                                |\n", VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
-        printk("| help - lists all commands                          |\n", VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
-        printk("| sysinfo - prints kernel version                    |\n", VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
-        printk("| clear - clears the screen                          |\n", VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
-        printk("| panic - tells os to crash, only for testing        |\n", VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
-        printk("|                                                    |\n", VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
-        printk("| setcolor - sets text and background color          |\n", VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
-        printk("| takes 2 parameters first one is text color         |\n", VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
-        printk("| second one is background color                     |\n", VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
-        printk("| not implemented yet                                |\n", VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
-        printk("------------------------------------------------------\n", VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
-        printk("myos > ", VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
-    }
-    else if (strcmp(buffer, "sysinfo\n") == 0) {
-        printk("\n", 15, 0);
-        printk("kernel version: 0.0.1\n", VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
-        printk("myos > ", VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
-    }
-    else if (strcmp(buffer, "clear\n") == 0) {
-        clear(VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
-        printk("myos > ", VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
+    struct tokenType tokenType;
+
+    char *token = strtok(buffer, " ");
+    char *args = strtok(NULL, "");
+
+    tokenType.tokens = get_token(token);
+    tokenType.parameter = args;
+
+    switch (tokenType.tokens) {
+        case TOKEN_ECHO:
+            printk(args, VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
+            break;
     }
 
-    else if (strcmp(buffer, "panic\n") == 0) {
-        kernel_panic();
-    }
-    else if (strcmp(buffer, "readdisk")) {
-        uint8_t buffer[512];
-        read_disk(2, buffer);
-
-        for (size_t i = 0;i < 512;i++) {
-            char_printk(buffer[i], VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
-        }
-    }
-    else if (strcmp(buffer, "\n") == 0) {
-
-    }
-    else {
-        printk("invalid command \n", VGA_COLOR_LIGHT_GREY ,VGA_COLOR_BLACK);
-        printk("myos > ", VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
-    }
 }
--- kernel/strcmp.h
+++ kernel/strcmp.h
@@ -20,5 +20,61 @@ int strncmp(const char *a, const char *b, size_t n) {
     return 0;
 }
 
+char* strtok(char* str, const char* delim) {
+    static char* next = 0;  // Keeps track of next position
+    char* token_start;
+    int i;
+
+    if (str)
+        next = str;  // Initialize on first call
+
+    if (!next)
+        return 0;    // No more tokens
+
+    // Skip leading delimiters
+    while (*next) {
+        int is_delim = 0;
+        for (i = 0; delim[i]; i++) {
+            if (*next == delim[i]) {
+                is_delim = 1;
+                break;
+            }
+        }
+        if (!is_delim)
+            break;
+        next++;
+    }
+
+    if (*next == 0) {
+        next = 0;
+        return 0;
+    }
+
+    // Start of token
+    token_start = next;
+
+    // Find the end of the token
+    while (*next) {
+        int is_delim = 0;
+        for (i = 0; delim[i]; i++) {
+            if (*next == delim[i]) {
+                is_delim = 1;
+                break;
+            }
+        }
+        if (is_delim) {
+            *next = 0;  // Terminate token
+            next++;
+            return token_start;
+        }
+        next++;
+    }
+
+    // Last token
+    next = 0;
+    return token_start;
+}
+
+
 
 #endif //STRCMP_H
